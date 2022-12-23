7f5b8ca1a0ef48088e493455b8239c0b104b1d21
DaniilTulkin <tulkindaniilal@gmail.com>
2022-12-23 18:08:04 +0400

    Add project files.

---
+++ ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace wpf_advance
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ ViewModel/WindowViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace wpf_advance
+{
+    public class WindowViewModel : BaseViewModel
+    {
+        private Window window;
+        private int outerMarginSize = 10;
+        private int windowRadius = 10;
+
+        public double WindowMinimumWidth { get; set; } = 400;
+        public double WindowMinimumHeight { get; set; } = 400;
+        public int ResizeBorder { get; set; } = 6;
+        public Thickness ResizeBorderThickness
+        {
+            get { return new Thickness(ResizeBorder + OuterMarginSize); }
+        }
+        public Thickness InnerContentPadding
+        {
+            get { return new Thickness(ResizeBorder); }
+        }
+        public int OuterMarginSize
+        {
+            get { return window.WindowState == WindowState.Maximized ? 0 : outerMarginSize; }
+            set { outerMarginSize = value; }
+        }
+        public Thickness OuterMarginSizeThickness
+        {
+            get { return new Thickness(OuterMarginSize); }
+        }
+        public int WindowRadius
+        {
+            get { return window.WindowState == WindowState.Maximized ? 0 : windowRadius; }
+            set { windowRadius = value; }
+        }
+        public CornerRadius WindowCornerRadius
+        {
+            get { return new CornerRadius(WindowRadius); }
+        }
+        public int TitleHeight { get; set; } = 42;
+        public GridLength TitleHeightGridLength { get { return new GridLength(TitleHeight + ResizeBorder); } }
+        public WindowViewModel(Window window)
+        {
+            this.window = window;
+
+            window.StateChanged += (sender, e) => 
+            {
+                OnPropertyChanged(nameof(ResizeBorderThickness));
+                OnPropertyChanged(nameof(OuterMarginSize));
+                OnPropertyChanged(nameof(OuterMarginSizeThickness));
+                OnPropertyChanged(nameof(WindowRadius));
+                OnPropertyChanged(nameof(WindowCornerRadius));
+            };
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(ref Win32Point pt);
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Win32Point
+        {
+            public int X;
+            public int Y;
+        }
+        private static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+        public ICommand MinimizeCommand => new RelayCommand(() => window.WindowState = WindowState.Minimized);
+        public ICommand MaximizeCommand => new RelayCommand(() => window.WindowState ^= WindowState.Maximized);
+        public ICommand CloseCommand => new RelayCommand(() => window.Close());
+        public ICommand MenuCommand => new RelayCommand(() => SystemCommands.ShowSystemMenu(window, GetMousePosition()));
+    }
+}
