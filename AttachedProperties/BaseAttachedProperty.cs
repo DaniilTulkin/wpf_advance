622a456f9edb3ff746501f3e4681a1dd860d03cc
DaniilTulkin <tulkindaniilal@gmail.com>
2022-12-24 15:53:56 +0400

    BaseAttachedProperty

---
+++ AttachedProperties/BaseAttachedProperty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace wpf_advance
+{
+    public abstract class BaseAttachedProperty<Parent, Property>
+        where Parent : BaseAttachedProperty<Parent, Property>, new()
+    {
+        public event Action<DependencyObject, DependencyPropertyChangedEventArgs> ValueChanged = (sender,e) => { };
+        public static Parent Instance { get; private set; } = new Parent();
+        public static readonly DependencyProperty ValueProperty = 
+            DependencyProperty.RegisterAttached("Value", typeof(Property), typeof(BaseAttachedProperty<Parent, Property>), new PropertyMetadata(new PropertyChangedCallback(OnValuePropertyChanged)));
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Instance.OnValueChanged(d, e);
+            Instance.ValueChanged(d, e);
+        }
+        public static Property GetValue(DependencyObject d) => (Property)d.GetValue(ValueProperty);
+        public static void SetValue(DependencyObject d, Property value) => d.SetValue(ValueProperty, value);
+        public virtual void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+    }
+}
