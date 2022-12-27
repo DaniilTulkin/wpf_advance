4a2386c1b139d565faadee2445ca2f47f6d4d1e2
DaniilTulkin <tulkindaniilal@gmail.com>
2022-12-27 15:30:35 +0400

    ChatMessage

---
+++ Controls/Chat/ChatMessage/ChatMessageListControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace wpf_advance
+{
+    public partial class ChatMessageListControl : UserControl
+    {
+        public ChatMessageListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Controls/Chat/ChatMessage/ChatMessageListItemControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace wpf_advance
+{
+    public partial class ChatMessageListItemControl : UserControl
+    {
+        public ChatMessageListItemControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
